53c57550435785cd489cc974fac5a856d88c3c80
Dmitry Yakimenko <detunized@gmail.com>
2015-12-08 17:36:40 +0100

    Derive encryption key

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+
+gem "pbkdf2-ruby"
---
+++ dashlane.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Exploring Dashlane dump here
+
+require "pbkdf2"
+
+def derive_encryption_key password, salt
+    pbkdf2 = PBKDF2.new(password: password,
+                        salt: salt,
+                        iterations: 10204,
+                        key_length: 32,
+                        hash_function: :sha1)
+    pbkdf2.bin_string
+end
