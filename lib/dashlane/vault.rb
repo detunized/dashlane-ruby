8800c539e6d0c5cb344bbfef46032e54eabbd73b
Dmitry Yakimenko <detunized@gmail.com>
2016-01-13 18:57:48 +0100

    Minor renaming

--- lib/dashlane/vault.rb
+++ lib/dashlane/vault.rb
@@ -4,25 +4,25 @@
 module Dashlane
     class Vault
         def self.open_remote username, password, uki
-            blob = Fetcher.fetch username, uki
-            open blob, username, password
+            text = Fetcher.fetch username, uki
+            open text, username, password
         end
 
         def self.open_local filename, username, password
-            blob = File.read filename
-            open blob, username, password
+            text = File.read filename
+            open text, username, password
         end
 
-        def self.open blob, username, password
-            new blob
+        def self.open text, username, password
+            new text
         end
 
         def self.compute_encryption_key password, salt
             OpenSSL::PKCS5.pbkdf2_hmac_sha1 password, salt, 10204, 32
         end
 
-        def initialize blob
-            json = JSON.load blob
+        def initialize text
+            data = JSON.load text
         end
     end
 end
