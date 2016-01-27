2d82a1c3694d87bf5babc62768aed63340d2806c
Dmitry Yakimenko <detunized@gmail.com>
2016-01-27 02:02:23 +0100

    Removed dashlane.rb (ref #1)

--- reencrypt.rb
+++ reencrypt.rb
@@ -1,6 +1,73 @@
 #!/usr/bin/env ruby
 
-require_relative "dashlane"
+require "base64"
+require "json"
+require "openssl"
+
+SALT_LENGTH = 32
+VERSION_LENGTH = 4
+KWC3_VERSION = "KWC3"
+
+def compute_encryption_key password, salt
+    OpenSSL::PKCS5.pbkdf2_hmac_sha1 password, salt, 10204, 32
+end
+
+def parse_encrypted_blob blob
+    salt = blob[0, SALT_LENGTH]
+    version = blob[SALT_LENGTH, VERSION_LENGTH]
+
+    if version == KWC3_VERSION
+        {
+                       salt: salt,
+                 ciphertext: blob[SALT_LENGTH + VERSION_LENGTH .. -1],
+                 compressed: true,
+            use_derived_key: false,
+                 iterations: 1,
+                    version: version
+        }
+    else
+        {
+                       salt: salt,
+                 ciphertext: blob[SALT_LENGTH .. -1],
+                 compressed: false,
+            use_derived_key: true,
+                 iterations: 5,
+                    version: ""
+        }
+    end
+end
+
+def sha1 bytes, times
+    times.times do
+        bytes = Digest::SHA1.digest bytes
+    end
+
+    bytes
+end
+
+def derive_encryption_key_iv encryption_key, salt, iterations
+    salty_key = encryption_key + salt[0, 8]
+
+    parts = [""]
+    3.times do
+        parts << sha1(parts.last + salty_key, iterations)
+    end
+
+    key_iv = parts.join
+
+    {
+        key: key_iv[0, 32],
+        iv: key_iv[32, 16]
+    }
+end
+
+def decrypt_aes256 ciphertext, iv, encryption_key
+    aes = OpenSSL::Cipher::AES256.new :CBC
+    aes.decrypt
+    aes.key = encryption_key
+    aes.iv = iv
+    aes.update(ciphertext) + aes.final
+end
 
 def encrypt_aes256 plaintext, iv, encryption_key
     aes = OpenSSL::Cipher::AES256.new :CBC
